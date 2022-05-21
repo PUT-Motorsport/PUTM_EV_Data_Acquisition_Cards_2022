dc30e8b01a53c69cc8fa0de8d7c2e6128d740052
Molson <molson.and@gmail.com>
2022-05-21 15:14:27 +0200

    added CAN library

---
+++ Core/Inc/ism_task.cpp
@@ -0,0 +1,19 @@
+/*
+ * ism_task.cpp
+ *
+ *  Created on: May 21, 2022
+ *      Author: molso
+ */
+
+#include <ism_task.hpp>
+#include <ISM330DHCXSensor.hpp>
+
+ism_task::ism_task() {
+	// TODO Auto-generated constructor stub
+
+}
+
+ism_task::~ism_task() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ Core/Inc/ism_task.hpp
@@ -0,0 +1,17 @@
+/*
+ * ism_task.hpp
+ *
+ *  Created on: May 21, 2022
+ *      Author: molso
+ */
+
+#ifndef INC_ISM_TASK_HPP_
+#define INC_ISM_TASK_HPP_
+
+class ism_task {
+public:
+	ism_task();
+	virtual ~ism_task();
+};
+
+#endif /* INC_ISM_TASK_HPP_ */
